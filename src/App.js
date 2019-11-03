737c87f4135296556b6aca6bbf9285de2ad435af
Nathalia <nathreboucas5@gmail.com>
2019-11-03 16:57:33 -0300

    refatorando App.js, criando rotas e componente de Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
-
-import logo from './assets/logo.svg';
+import Routes from './routes';
 
 function App() {
   return (
     <div className="container">    
-      <img src={logo} alt="AirCnC"/>
-
-      <div className="content">
-        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
-
-        <form>
-          <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email" placeholder="Seu melhor e-mail"/>
-
-          <button type="submit" className="btn">Entrar</button>
-        </form>
-      </div>
+      <Routes />
     </div>
   );
 }
