e18afcabbd68f0b643e9f039e9ee7d3ff7b9cfdf
Nathalia <nathreboucas5@gmail.com>
2019-11-03 23:58:49 -0300

    criando novo spot

--- src/pages/new/index.js
+++ src/pages/new/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useMemo } from 'react';
 import camera from '../../assets/camera.svg';
+import api from '../../services/api';
 import './styles.css';
 
-export default function New() {
+export default function New({ history }) {
 
 	const [company, setCompany] = useState('');
 	const [techs, setTechs] = useState([]);
@@ -12,8 +13,21 @@ export default function New() {
 		return thumbnail ? URL.createObjectURL(thumbnail) : null
 	}, [thumbnail]);
 
-	function handleOnSubmit() {
+	async function handleOnSubmit(event) {
+		event.preventDefault();
 
+		const data = new FormData();
+		const user_id = localStorage.getItem('user');
+		data.append('thumbnail', thumbnail);
+		data.append('company', company);
+		data.append('techs', techs);
+		data.append('price', price);
+
+		await api.post('/spots', data, {
+			headers: { user_id }
+		});
+
+		history.push('/dashboard');
 	}
 
 	return (
